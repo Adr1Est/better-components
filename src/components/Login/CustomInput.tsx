c2020dba4b6417bb787d6d6ca795493e49d16a57
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-03-30 16:15:23 +0200

    mejorar password inputs

--- src/components/Login/CustomInput.tsx
+++ src/components/Login/CustomInput.tsx
@@ -1,4 +1,5 @@
-import type { ChangeEvent } from "react";
+import { Eye, EyeClosed } from "lucide-react";
+import { useState, type ChangeEvent } from "react";
 
 interface Props {
   label: string;
@@ -10,17 +11,33 @@ interface Props {
 }
 
 export default function CustomInput({ label, type, id, placeholder, value, handleChange }: Props){
+  const [passState, setPassState] = useState("password");
+
   return(
     <div className="flex flex-col">
       <label htmlFor={id} className="font-semibold text-secondary-400 text-sm">{label}</label>
-      <input 
-        className="bg-slate-950 h-10 p-5 rounded-md"
-        type={type}
-        id={id} 
-        placeholder={placeholder}
-        value={value}
-        onChange={handleChange}
-      />
+      <div className="relative w-full">
+        <input 
+          className="bg-slate-950 h-10 p-5 rounded-md w-full"
+          type={type === "password" ? passState : type}
+          id={id} 
+          placeholder={placeholder}
+          value={value}
+          onChange={handleChange}
+        />
+        {
+          type === "password"
+            ? (
+                <button 
+                  className="absolute right-2 top-2 hover:text-secondary-400"
+                  onClick={() => setPassState(p => p === "password" ? "text" : "password")}
+                >
+                  {passState === "password" ? <Eye /> : <EyeClosed />}
+                </button>
+              )
+            : ""
+        }
+      </div>
     </div>
   )
 }
