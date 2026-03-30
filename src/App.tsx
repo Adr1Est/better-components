acb026d1a8e30a55e2f15a75e5971d022558a159
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-03-30 13:33:12 +0200

    eliminar useRefreshToken hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import '@/App.css'
 import { Outlet } from 'react-router'
-import { useRefreshToken } from '@/hooks/useRefreshToken'
 
 function App() {
-  useRefreshToken();
-
+  
   return (
     <Outlet />
   )
