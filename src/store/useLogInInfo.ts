71346074d876ac1c1e6d688db0c3f3cd6639d0c0
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-03-30 15:33:14 +0200

    userId store

--- src/store/useLogInInfo.ts
+++ src/store/useLogInInfo.ts
@@ -2,12 +2,18 @@ import { create } from "zustand";
 
 interface LogInInfoStore {
   accessToken: string;
+  userId: string;
   setAccessToken: (token: string) => void;
   deleteAccessToken: () => void;
+  setUserId: (id: string) => void;
+  deleteUserId: () => void;
 }
 
 export const useLogInInfo = create<LogInInfoStore>((set) => ({
   accessToken: "",
+  userId: "",
   setAccessToken: (token: string) => set(() => ({ accessToken: token })),
   deleteAccessToken: () => set(() => ({ accessToken: "" })),
+  setUserId: (id: string) => set(() => ({ userId: id })),
+  deleteUserId: () => set(() => ({ userId: "" })),
 }));
