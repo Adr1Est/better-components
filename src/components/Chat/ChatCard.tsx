54f313e5eefae84c4a82514cc5f073fa77a0545a
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-04-06 11:08:38 +0200

    modal para borrar chats

--- src/components/Chat/ChatCard.tsx
+++ src/components/Chat/ChatCard.tsx
@@ -1,7 +1,8 @@
-import { useDeleteChat } from "@/hooks/useConversation";
 import dayjs from "@/utils/dayjs";
 import { Pencil, Trash } from "lucide-react";
 import { Link } from "react-router";
+import DeleteChatModal from "@/components/Chat/DeleteChatModal";
+import { useRef } from "react";
 
 interface Props {
   id: string;
@@ -10,39 +11,54 @@ interface Props {
 }
 
 export default function ChatCard({ id, title, createdAt }: Props) {
-  const { mutate: deleteChat } = useDeleteChat();
+  const modalRef = useRef<HTMLDialogElement>(null);
 
   const handleEdit = async () => {
     console.log(id);
   }
 
-  const handleDelete = () => {
-    deleteChat(id);
+  const handleModalOpen = () => {
+    modalRef.current!.showModal();
   }
 
   return (
-    <Link 
-      className="flex flex-row gap-3 justify-center border border-tertiary-700 bg-surface-900 hover:bg-surface-600 rounded-xl p-3 w-80 md:w-100 h-40 transition-colors duration-500"
-      to={`/chat/${id}`}
-    >
-      <h2 className="text-xl font-bold w-1/2">{title}</h2>
-      <div className="flex flex-col w-1/2 h-full justify-end items-end">
-        <div className="flex flex-row gap-1">
-          <button 
-            className="w-20 flex items-center justify-center p-3 bg-surface-950 hover:bg-secondary-900 rounded-xl group transition-colors duration-500"
-            onClick={handleEdit}
-          >
-            <Pencil className="group-hover:scale-115 transition-transform" />
-          </button>
-          <button 
-            className="w-20 flex items-center justify-center p-3 bg-surface-950 hover:bg-red-900 rounded-xl group transition-colors duration-500"
-            onClick={handleDelete}
-          >
-            <Trash className="group-hover:scale-115 transition-transform" />
-          </button>
+    <>
+      <Link 
+        className="flex flex-row gap-3 justify-center border border-tertiary-700 bg-surface-900 hover:bg-surface-600 rounded-xl p-3 w-80 md:w-100 h-40 transition-colors duration-500"
+        to={`/chat/${id}`}
+      >
+        <h2 className="text-xl font-bold w-1/2">{title}</h2>
+        <div className="flex flex-col w-1/2 h-full justify-end items-end">
+          <div className="flex flex-row gap-1">
+            <button 
+              className="w-20 flex items-center justify-center p-3 bg-surface-950 hover:bg-secondary-900 rounded-xl group transition-colors duration-500"
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                handleEdit();
+              }}
+            >
+              <Pencil className="group-hover:scale-115 transition-transform" />
+            </button>
+            <button 
+              className="w-20 flex items-center justify-center p-3 bg-surface-950 hover:bg-red-900 rounded-xl group transition-colors duration-500"
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                handleModalOpen();
+              }}
+            >
+              <Trash className="group-hover:scale-115 transition-transform" />
+            </button>
+          </div>
+          <p className="opacity-50 text-sm">{dayjs(createdAt).fromNow()}</p>
         </div>
-        <p className="opacity-50 text-sm">{dayjs(createdAt).fromNow()}</p>
-      </div>
-    </Link>
+      </Link>
+      <DeleteChatModal 
+        ref={modalRef}
+        title={title}
+        chatId={id}
+      />
+    </>
   )
 }
---
+++ src/components/Chat/DeleteChatModal.tsx
@@ -0,0 +1,42 @@
+import { useDeleteChat } from "@/hooks/useConversation";
+import { X, Trash } from "lucide-react";
+import type { RefObject } from "react";
+
+interface Props {
+  ref: RefObject<HTMLDialogElement | null>;
+  title: string;
+  chatId: string;
+}
+
+export default function DeleteChatModal({ ref, title, chatId }: Props) {
+  const { mutate: deleteChat } = useDeleteChat();
+  
+  const handleDelete = () => {
+    deleteChat(chatId);
+    ref.current?.close();
+  }
+
+  return(
+    <dialog 
+      ref={ref}
+      className="hidden open:flex flex-col gap-2 w-90 m-auto rounded-xl p-3 bg-surface-800 border-3 border-tertiary-800 text-tertiary-400 font-semibold backdrop:bg-black/50"
+    >
+      <p>Quieres borrar este chat?</p>
+      <p className="w-full text-secondary-400 font-bold bg-surface-900 p-1 rounded-xl">{title}</p>
+      <div className="flex flex-row gap-1 w-full justify-end">
+        <button
+          onClick={() => ref.current?.close()}
+          className="bg-surface-900 rounded-xl p-1 hover:text-text-primary"
+        >
+          <X strokeWidth={3}/>
+        </button>
+        <button
+          onClick={handleDelete}
+          className="bg-surface-900 rounded-xl p-1 hover:text-text-primary"
+        >
+          <Trash strokeWidth={3}/>
+        </button>
+      </div>
+    </dialog>
+  )
+}
