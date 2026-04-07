7c802f9fe2a3806c741aff443c2a7e07dea1e580
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-04-07 13:42:49 +0200

    useSaveApiKey hook

--- src/hooks/useUserInfo.ts
+++ src/hooks/useUserInfo.ts
@@ -1,10 +1,22 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { getUserInfo } from "@/services/user";
+import { saveApiKey } from "@/services/key";
 
 export const useUserInfo = (userId: string) => {
   return useQuery({
     queryKey: ["user", userId],
     queryFn: () => getUserInfo(userId),
     enabled: !!userId,
+  });
+}
+
+export const useSaveApiKey = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ id, apiKey }: { id: string; apiKey: string;}) => saveApiKey(id, apiKey),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+    },
   });
 }
