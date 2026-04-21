19a8d93d11e0625cd12f2d55e49c0e2a0e147b62
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-04-21 10:34:12 +0200

    deleteUserData

--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -4,3 +4,8 @@ export const getUserInfo = async (id: string) => {
   const response = await api.get(`/user/${id}`);
   return response.data;
 }
+
+export const deleteUserData = async () => {
+  const response = await api.delete("/user");
+  return response.data;
+}
