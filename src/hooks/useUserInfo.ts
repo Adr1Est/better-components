325fce3554c182041f7924c39ebbd6024eaee72e
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-04-21 10:40:05 +0200

    useDeleteUser

--- src/hooks/useUserInfo.ts
+++ src/hooks/useUserInfo.ts
@@ -1,6 +1,8 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { getUserInfo } from "@/services/user";
+import { deleteUserData, getUserInfo } from "@/services/user";
 import { saveApiKey } from "@/services/key";
+import toast from "react-hot-toast";
+import { errorToast, successToast } from "@/utils/toasts";
 
 export const useUserInfo = (userId: string) => {
   return useQuery({
@@ -17,6 +19,21 @@ export const useSaveApiKey = () => {
     mutationFn: ({ id, apiKey }: { id: string; apiKey: string;}) => saveApiKey(id, apiKey),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["user"] });
+    },
+  });
+}
+
+export const useDeleteUser = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: deleteUserData,
+    onSuccess: () => {
+      queryClient.clear();
+      toast.success("Cuenta y datos borrados", successToast);
+    },
+    onError: (error: any) => {
+      toast.error(error?.response?.data?.msg || "Error al eliminar cuenta", errorToast);
     },
   });
 }
