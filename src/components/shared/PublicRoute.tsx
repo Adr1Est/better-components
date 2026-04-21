521399aac7ef605a37889e044702c8fe01093e6e
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-04-21 11:06:49 +0200

    Componente PublicRoute para inicio de sesión automático

---
+++ src/components/shared/PublicRoute.tsx
@@ -0,0 +1,40 @@
+import { refreshToken } from "@/services/auth";
+import { useLogInInfo } from "@/store";
+import { useEffect, useState } from "react";
+import { Navigate, Outlet } from "react-router";
+import FullScreenLoader from "@/components/shared/FullScreenLoader";
+
+export default function PublicRoute() {
+  const token = useLogInInfo((state) => state.accessToken);
+  const setToken = useLogInInfo((state) => state.setAccessToken);
+  const setUserId = useLogInInfo((state) => state.setUserId);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isAuth, setIsAuth] = useState(false);
+
+  useEffect(() => {
+    const refresh = async () => {
+      try {
+        if (token) {
+          setIsAuth(true);
+          setIsLoading(false);
+          return;
+        }
+
+        const data = await refreshToken();
+        setToken(data.token);
+        setUserId(data.id);
+        setIsAuth(true);
+      } catch {
+        setIsAuth(false);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    refresh();
+  }, []);
+
+  if (isLoading) return <FullScreenLoader />;
+
+  return isAuth ? <Navigate to="/dashboard" /> : <Outlet />;
+}
