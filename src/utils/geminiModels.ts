7e975112f5af7741b6fab4020d219698d8c314d0
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-05-13 17:33:54 +0200

    sustituir modelo gemini-3.1-flash-lite-preview por gemini-3.1-flash-lite

--- src/utils/geminiModels.ts
+++ src/utils/geminiModels.ts
@@ -1,5 +1,5 @@
 export const geminiModels = [
-  { id: 1, model: "gemini-3.1-flash-lite-preview" },
+  { id: 1, model: "gemini-3.1-flash-lite" },
   { id: 2, model: "gemini-3.1-pro-preview" },
   { id: 3, model: "gemini-3-flash-preview" },
 ]
